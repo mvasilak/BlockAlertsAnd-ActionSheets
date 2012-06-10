7a775ff00a1cc820f6e5f36f2d29aa4552e8b8c5
Steven O'Leary <soleary80@gmail.com>
2012-06-10 22:46:16 +0100

    Added BlockAlertViewQueue

---
+++ BlockAlertsDemo/ToAddToYourProjects/BlockAlertViewQueue.h
@@ -0,0 +1,12 @@
+//
+// Created by stevenoleary on 10/06/2012.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import <Foundation/Foundation.h>
+
+
+@interface BlockAlertViewQueue : NSObject
+@end
---
+++ BlockAlertsDemo/ToAddToYourProjects/BlockAlertViewQueue.m
@@ -0,0 +1,15 @@
+//
+// Created by stevenoleary on 10/06/2012.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import "BlockAlertViewQueue.h"
+
+
+@implementation BlockAlertViewQueue {
+
+}
+
+@end
