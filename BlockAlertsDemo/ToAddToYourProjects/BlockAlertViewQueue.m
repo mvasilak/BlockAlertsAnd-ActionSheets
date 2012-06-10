7d80845c4975d8833670066154b23da8ad648f93
Steven O'Leary <soleary80@gmail.com>
2012-06-10 23:01:05 +0100

    Add queueAlerts NSMutableArray and initialize

--- BlockAlertsDemo/ToAddToYourProjects/BlockAlertViewQueue.m
+++ BlockAlertsDemo/ToAddToYourProjects/BlockAlertViewQueue.m
@@ -9,18 +9,29 @@
 
 
 @implementation BlockAlertViewQueue {
-
+     NSMutableArray *queuedAlerts;
 }
+
 + (BlockAlertViewQueue *)instance {
     static BlockAlertViewQueue *_instance = nil;
 
     @synchronized (self) {
         if (_instance == nil) {
             _instance = [[self alloc] init];
+
         }
     }
 
     return _instance;
+}
+
+- (id)init {
+    queuedAlerts = [[NSMutableArray alloc] init];
+}
+
+- (void)dealloc {
+    [queuedAlerts release];
+    [super dealloc];
 }
 
 @end
