4d2aa9ca5bb8e6fe1c55116a4650fe5016005633
steveoleary <soleary80@gmail.com>
2012-06-12 01:27:44 +0200

    Update master

--- BlockAlertsDemo/ToAddToYourProjects/BlockAlertViewQueue.h
+++ BlockAlertsDemo/ToAddToYourProjects/BlockAlertViewQueue.h
@@ -1,10 +1,3 @@
-//
-// Created by stevenoleary on 10/06/2012.
-//
-// To change the template use AppCode | Preferences | File Templates.
-//
-
-
 #import <Foundation/Foundation.h>
 #import "BlockAlertView.h"
 
